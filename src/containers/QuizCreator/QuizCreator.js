b03458335d8c212620b74b6ae64c5c5398a6ca34
alex.paclin <alex.paclin@gmail.com>
2020-05-18 14:24:47 +0500

    6. Создание тестов

--- src/containers/QuizCreator/QuizCreator.js
+++ src/containers/QuizCreator/QuizCreator.js
@@ -1,9 +1,36 @@
+import { Button } from '../../components';
 import React from 'react';
+import s from './QuizCreator.module.scss';
 
 const QuizCreator = () => {
+	const submit = (event) => {
+		event.preventDefault();
+	};
+
+	const addQuestion = () => {};
+
+	const createQuiz = () => {};
+
 	return (
-		<div>
-			<h1>QuizCreator</h1>
+		<div className={s.QuizCreator}>
+			<div>
+				<h1>Quiz Creator</h1>
+				<form onSubmit={submit}>
+					<input type="text" />
+					<hr />
+					<input type="text" />
+					<input type="text" />
+					<input type="text" />
+					<input type="text" />
+					<select name="" id=""></select>
+					<Button type="primary" onClick={addQuestion}>
+						Add question
+					</Button>
+					<Button type="success" onClick={createQuiz}>
+						Create quiz
+					</Button>
+				</form>
+			</div>
 		</div>
 	);
 };
