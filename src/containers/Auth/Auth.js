5da63ff201e6457f412897a751ad01ca2c3bf128
alex.paclin <alex.paclin@gmail.com>
2020-05-18 11:20:29 +0500

    3. Валидация. Часть 1

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -1,9 +1,41 @@
+import React, { useState } from 'react';
+
 import { Button } from '../../components';
 import { Input } from '../../components';
-import React from 'react';
 import s from './Auth.module.scss';
 
 const Auth = () => {
+	const [formControls, setFormControls] = useState({
+		email: {
+			value: '',
+			type: 'email',
+			label: 'Email',
+			error: 'Please enter correct e-mail',
+			valid: false,
+			touched: false,
+			validation: {
+				required: true,
+				email: true,
+			},
+		},
+		password: {
+			value: '',
+			type: 'password',
+			label: 'Password',
+			error: 'Please enter correct password',
+			valid: false,
+			touched: false,
+			validation: {
+				required: true,
+				minLength: 6,
+			},
+		},
+	});
+
+	const change = (event, controlName) => {
+		console.log(`${controlName}: ${event.target.value}`);
+	};
+
 	const login = () => {};
 
 	const register = () => {};
@@ -12,13 +44,31 @@ const Auth = () => {
 		event.preventDefault();
 	};
 
+	const renderInputs = () => {
+		return Object.keys(formControls).map((controlName, index) => {
+			const control = formControls[controlName];
+			return (
+				<Input
+					key={controlName + index}
+					value={control.value}
+					type={control.type}
+					label={control.label}
+					error={control.error}
+					valid={control.valid}
+					touched={control.touched}
+					shouldValidate={!!control.validation}
+					onChange={(event) => change(event, controlName)}
+				/>
+			);
+		});
+	};
+
 	return (
 		<div className={s.Auth}>
 			<div>
 				<h1>Authorization</h1>
 				<form onSubmit={submit} className={s.AuthForm}>
-					<Input label="Email"/>
-					<Input label="Password" error="TEST"/>
+					{renderInputs()}
 					<Button type="success" onClick={login}>
 						Log In
 					</Button>
